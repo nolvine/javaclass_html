893b6cbcbcaa2e37bdd881b9f1b70341a3aa576a
nolvine <nolvine234@gmail.com>
2024-10-16 15:43:25 +0300

    initial commit

---
+++ imagefi/js/main.js
@@ -0,0 +1,7 @@
+const javaclass=document.getElementById("colorButton");
+
+// add event listener for the button click
+javaclass.addEventListener("click", function(){})
+
+// change the color of the button
+javaclass.style.backgroundColor=javaclass.style.backgroundColor=javaclass.style.backgroundColor="blue";
